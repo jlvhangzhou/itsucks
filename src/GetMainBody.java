4e417159e4b0e383875c98b9850217fdbd0726bd
Wu Hualiang <wizawu@gmail.com>
2012-08-18 20:38:05 +0800

    crawler getText fixed

---
+++ src/GetMainBody.java
@@ -0,0 +1,19 @@
+/**
+ *  @author Wu Hualiang <wizawu@gmail.com>
+ *  从parse后的网页纯文本提取正文内容
+ */
+
+public class GetMainBody {
+	
+	static public int blockThi
+	
+	static public String getResult(String text) {
+		String[] lines = text.split("\n");
+		int totalLines = lines.length;
+		int 
+		String result = null;
+		
+		return result;
+	}
+
+}
